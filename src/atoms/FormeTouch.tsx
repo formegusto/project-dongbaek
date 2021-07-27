657a21cfd0a77de39920d1874fb7359d8c10b434
formegusto <formegusto@gmail.com>
2021-07-27 23:58:58 +0900

    animation

--- src/atoms/FormeTouch.tsx
+++ src/atoms/FormeTouch.tsx
@@ -1,19 +1,42 @@
-import styled from "styled-components";
+import React from "react";
+import styled, { css, keyframes } from "styled-components";
 
-function FormeTouch() {
+type Props = {
+  changeAllWhite: () => void;
+};
+
+function FormeTouch({ changeAllWhite }: Props) {
+  const refFlashBlock = React.useRef<HTMLDivElement>(null);
+  const refBody = React.useRef<HTMLDivElement>(null);
+  const [borderAni, setBorderAni] = React.useState<boolean>(false);
+
+  React.useEffect(() => {
+    if (refFlashBlock) {
+      refFlashBlock.current?.addEventListener("animationend", () => {
+        setBorderAni(true);
+      });
+    }
+
+    if (refBody) {
+      refBody.current?.addEventListener("animationend", () => {
+        changeAllWhite();
+      });
+    }
+  }, [changeAllWhite]);
+
   return (
     <>
-      <FlashBlock />
-      <Body>
-        <LensBlock>
-          <Lens></Lens>
+      <FlashBlock ref={refFlashBlock} ani={borderAni} />
+      <Body ani={borderAni} ref={refBody}>
+        <LensBlock ani={borderAni}>
+          <Lens ani={borderAni} />
         </LensBlock>
       </Body>
     </>
   );
 }
 
-const AniFlashBlock = styled.div`
+const AniFlashBlock = keyframes`
   from {
     transform: translateY(0);
   }
@@ -22,7 +45,24 @@ const AniFlashBlock = styled.div`
   }
 `;
 
-const Body = styled.div`
+const AniFlashBlockBorder = keyframes`
+  from {
+    transform: translateY(-75px);
+  }
+  to {
+    border: 2px solid #000;
+    border-bottom: none;
+    transform: translateY(-75px);
+  }
+`;
+
+const AniBorderWidth = keyframes`
+  to {
+    border: 2px solid #000;
+  }
+`;
+
+const Body = styled.div<{ ani?: boolean }>`
   z-index: 2;
   position: relative;
 
@@ -37,9 +77,16 @@ const Body = styled.div`
   box-sizing: border-box;
   border-radius: 32px;
   background-color: #000;
+
+  ${(props) =>
+    props.ani &&
+    css`
+      animation: ${AniBorderWidth} 1s forwards;
+      background-color: transparent;
+    `}
 `;
 
-const FlashBlock = styled.div`
+const FlashBlock = styled.div<{ ani?: boolean }>`
   position: absolute;
   z-index: 1;
   top: 0;
@@ -49,13 +96,20 @@ const FlashBlock = styled.div`
   height: 75px;
 
   border: 6px solid #ffffff;
+  border-bottom: none;
   box-sizing: border-box;
   border-radius: 16px 16px 0 0;
 
-  animation: ${AniFlashBlock} 0.7s forwards;
+  animation: ${AniFlashBlock} 1.2s forwards;
+
+  ${(props) =>
+    props.ani &&
+    css`
+      animation: ${AniFlashBlockBorder} 1s forwards;
+    `}
 `;
 
-const LensBlock = styled.div`
+const LensBlock = styled.div<{ ani?: boolean }>`
   display: flex;
   justify-content: center;
   align-items: center;
@@ -66,15 +120,27 @@ const LensBlock = styled.div`
   border: 6px solid #ffffff;
   box-sizing: border-box;
   border-radius: 100%;
+
+  ${(props) =>
+    props.ani &&
+    css`
+      animation: ${AniBorderWidth} 1s forwards;
+    `}
 `;
 
-const Lens = styled.div`
+const Lens = styled.div<{ ani?: boolean }>`
   width: 135px;
   height: 135px;
 
   border: 6px solid #ffffff;
   box-sizing: border-box;
   border-radius: 100%;
+
+  ${(props) =>
+    props.ani &&
+    css`
+      animation: ${AniBorderWidth} 1s forwards;
+    `}
 `;
 
 export default FormeTouch;
--- src/components/DongbaekComponent.tsx
+++ src/components/DongbaekComponent.tsx
@@ -1,15 +1,33 @@
-import styled from "styled-components";
+import React from "react";
+import { useCallback } from "react";
+import styled, { css, keyframes } from "styled-components";
 import FormeTouch from "../atoms/FormeTouch";
 
 function DongbaekComponent() {
+  const [allWhite, setAllWhite] = React.useState<boolean>(false);
+
+  const changeAllWhite = useCallback(() => {
+    setAllWhite(true);
+  }, []);
+
   return (
     <DongbaekScreen>
+      <DongbaekScreenWhite ani={allWhite} />
       <PolarBlock>
-        <FormeTouch />
+        <FormeTouch changeAllWhite={changeAllWhite} />
       </PolarBlock>
     </DongbaekScreen>
   );
 }
+
+const AniWhiteScreen = keyframes`
+  from {
+    border-radius: 100%;
+    trasform: scale(0);
+  } to {
+    transform: scale(1);
+  }
+`;
 
 const DongbaekScreen = styled.div`
   display: flex;
@@ -20,6 +38,25 @@ const DongbaekScreen = styled.div`
   height: 100%;
 
   background-color: #000;
+`;
+
+const DongbaekScreenWhite = styled.div<{ ani?: boolean }>`
+  position: absolute;
+  top: 0;
+  left: 0;
+
+  width: 100%;
+  height: 100%;
+
+  background-color: #fff;
+
+  transform: scale(0);
+
+  ${(props) =>
+    props.ani &&
+    css`
+      animation: ${AniWhiteScreen} 0.7s forwards;
+    `}
 `;
 
 const PolarBlock = styled.div`
